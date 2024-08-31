00f46e5811fce9c31fd2891e6bd4410a9e34c793
Luis Beu <me@luisbeu.de>
2024-09-01 00:10:15 +0200

    Add pointer class

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -7,7 +7,6 @@ import {
 import { cx } from '@twind/core';
 import { type ComponentPropsWithoutRef, type ElementRef, forwardRef } from 'react';
 
-// TODO: Cursor
 const Slider = forwardRef<
     ElementRef<typeof SliderPrimitiveRoot>,
     ComponentPropsWithoutRef<typeof SliderPrimitiveRoot>
@@ -17,10 +16,10 @@ const Slider = forwardRef<
         ref={ref}
         {...props}
     >
-        <SliderPrimitiveTrack className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+        <SliderPrimitiveTrack className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary cursor-pointer">
             <SliderPrimitiveRange className="absolute h-full bg-primary" />
         </SliderPrimitiveTrack>
-        <SliderPrimitiveThumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+        <SliderPrimitiveThumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer" />
     </SliderPrimitiveRoot>
 ));
 
